720f6970a70525df41a77974f722b1bd0348eae6
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-04-12 11:58:31 +0200

    configurando urls.py

--- locallibrary/locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/locallibrary/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+
+#Add URL maps to redirect the base URL to our application
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
